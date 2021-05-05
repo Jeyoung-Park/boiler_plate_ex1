e4e5fa5139e14a990196b244d94f30674fb7c6d2
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-05-05 21:58:18 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const app=express();
 const port=5000;
+const bodyParser=require('body-parser');
+const {User}=require('./models/User');
+
+const config=require('./config/key');
+
+//application/x-www-form-urlencoded 으로 된 것을 가져올 수 있음
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json으로 된 것들을 가져올 수 있음
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://jeyoungPark:qwer1234*@boilerplate.0lx7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'.{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
@@ -12,8 +22,23 @@ mongoose.connect('mongodb+srv://jeyoungPark:qwer1234*@boilerplate.0lx7g.mongodb.
 .then(()=>{
     console.log('MongoDB connected...');
 })
-.catch(err=>console.log(err));
+.catch(err=>console.log(err)); 
 
-app.get('/', (req, res)=>res.send('Hello world!22222222222'))
+app.get('/', (req, res)=>res.send('Hello world~~~~~~!'))
+
+app.post('/register', (req, res)=>{
+    //회원 가입 할 떄 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다
+    
+    const user=new User(req.body);
+
+    //정보들을 유저 모델에 저장
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success:false, err});
+        return res.status(200).json({
+            success:true,
+        })
+    });
+})
 
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`))
